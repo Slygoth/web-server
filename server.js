124ee288ab1e461f27ad992bd4fb8179f05a10a6
Nicolaus Lawson <nicolaus.lawson@gmail.com>
2016-12-20 15:10:20 -0500

    more update

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var middleware = {
-  requireAuthentication: function(req, res, next) {
-      console.log('private route hit!');
-      next();
-  },
-  logger: function (req, res, next) {
-
-    console.log('Request: ' + new Date().toString() + req.method + ' ' + req.originalUrl + '  Date of Request: ');
-    next();
-  }
-};
+var middleware = require('./middleware.js');
 
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
